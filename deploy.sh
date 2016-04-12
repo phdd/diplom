fe105ee7e2f851e194ecf4e64ffe915aabc33e4a
Peter Heisig <peter.heisig@medien-dresden.de>
2016-04-12 11:46:49 +0200

    Deployment

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-git config user.name "Travis CI"
-git config user.email "joe@travis-ci.org"
+git config --global user.name "Travis CI"
+git config --global user.email "joe@travis-ci.org"
 
 git clone "https://${GH_TOKEN}@${GH_REF}" pages
-git checkout gh-pages
-
-cp document.html pages/index.html
-cp document.pdf  pages/thesis.pdf
-cp document.epub pages/thesis.epub
 
 cd pages
+
+git checkout gh-pages
+
+cp ../document.html index.html
+cp ../document.pdf  thesis.pdf
+cp ../document.epub thesis.epub
 
 git add .
 git commit -m "Deploy to GitHub Pages"
